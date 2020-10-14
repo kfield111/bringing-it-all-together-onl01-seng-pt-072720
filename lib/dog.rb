a2bb75585b3cf0337b329eb7ae37662d5f3d3821
Kelin Field <kfield111@hotmail.com>
2020-10-14 00:16:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,9 +40,10 @@ class Dog
         self
     end
 
-    def self.create (name, breed)
+    def self.create (name:, breed:)
       dog = Dog.new(name, breed)
       dog.save
+      dog
     end
 
 
