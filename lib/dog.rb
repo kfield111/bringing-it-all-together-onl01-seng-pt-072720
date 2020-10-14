95771be2d422466186c8b17ef3020d86400374bc
Kelin Field <kfield111@hotmail.com>
2020-10-14 00:48:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,6 +86,7 @@ class Dog
 
     def find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-
+      if !dogs.empty?
+        dog_data = dog[0]
 
 end
